3de1d314d2358340406811785f2dedbe43ad11ed
Anastasiya Machulskaya <94453559+Lossteg@users.noreply.github.com>
2024-11-14 16:44:26 +0300

    add task-1 implementation

---
+++ Autoloading-PSR/Task-1/Animals/Cat.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Animals;
+class Cat implements IAnimal
+{
+    public function makeSound(): void
+    {
+        echo "Мяу-мяу" . PHP_EOL;
+    }
+    public function eat(): void
+    {
+        echo "Ест корм для котов" . PHP_EOL;
+    }
+    public function sleep(): void
+    {
+        echo "Cпит в коробке" . PHP_EOL;
+    }
+}
